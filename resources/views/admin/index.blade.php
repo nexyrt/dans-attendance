bbf5d26d4015f0214a7aec493f2a4dd2fb21e1f7
Rizky Hamdani <rizkyyhamdani21@gmail.com>
2024-05-16 19:12:40 +0800

    Remake UI

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,6 +1,6 @@
 <x-admin-layout>
 
-    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <div class="relative overflow-x-auto mt-4 shadow-md sm:rounded-lg">
         <div
             class="flex items-center justify-between flex-column flex-wrap md:flex-row space-y-4 md:space-y-0 pb-4 bg-white dark:bg-gray-900 p-5">
             <div>
