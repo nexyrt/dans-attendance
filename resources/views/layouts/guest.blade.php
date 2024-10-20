95342fd32d5e0244403fbe9f3eff4b5496606430
Rizky Hamdani <Rizky Hamdani>
2024-10-21 07:10:43 +0800

    unnecessary changes

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -9,6 +9,7 @@
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
+        <link rel="icon" href="{{ asset('images/dans.png') }}">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
