1f811300a7f3f69d0ee7277af046471a5106061a
Rizky Hamdani <rizkyyhamdani21@gmail.com>
2024-11-23 01:34:54 +0800

    "Updated ScheduleCalendar Livewire component, added onEventClick and selectSchedule methods, modified index.blade.php and schedule-calendar.blade.php views, and updated composer.lock file."

--- app/Livewire/Admin/Schedules/ScheduleCalendar.php
+++ app/Livewire/Admin/Schedules/ScheduleCalendar.php
@@ -10,19 +10,35 @@ use Omnia\LivewireCalendar\LivewireCalendar;
 
 class ScheduleCalendar extends LivewireCalendar
 {
+    public $schedule = false;
     public function onEventDropped($eventId, $year, $month, $day)
     {
         ScheduleException::where('id', $eventId)
-        ->update([
-            'date' => $year . '-' . $month . '-' . $day
-        ]);
+            ->update([
+                'date' => $year . '-' . $month . '-' . $day
+            ]);
     }
 
-    public function nextMonth(){
+    public function onEventClick($eventId)
+    {
+        // This event is triggered when an event card is clicked
+        // You will be given the event id that was clicked
+        $schedule = ScheduleException::find($eventId);
+        $this->dispatchBrowserEvent('schedule-selected', $schedule);
+    }
+
+    public function selectSchedule($schedule)
+    {
+        // Dispatch a browser event with the schedule data
+    }
+
+    public function nextMonth()
+    {
         $this->goToNextMonth();
     }
 
-    public function prevMonth(){
+    public function prevMonth()
+    {
         $this->goToPreviousMonth();
     }
 
--- resources/views/admin/schedule/index.blade.php
+++ resources/views/admin/schedule/index.blade.php
@@ -1,6 +1,18 @@
 <x-layouts.admin>
-    <livewire:admin.schedules.schedule-calendar
-        {{-- day-view="calendar/event-view" --}}
-        before-calendar-view="calendar/before-calendar-view"
-    />
+    <div class="flex w-full gap-4" x-data="{
+        schedule: null,
+        updateSchedule(data) {
+            this.schedule = data;
+            $wire.handleScheduleUpdate(data);
+        }
+    }">
+        <div class="basis-3/4">
+            <livewire:admin.schedules.schedule-calendar {{-- day-view="calendar/event-view" --}}
+                before-calendar-view="calendar/before-calendar-view" />
+        </div>
+        <div class="basis-1/4 w-full border shadow-md bg-white rounded-md h-max">
+            {{-- {{ $this->schedule }} --}}
+        </div>
+    </div>
+
 </x-layouts.admin>
