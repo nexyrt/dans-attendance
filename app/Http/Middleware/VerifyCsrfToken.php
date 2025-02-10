f8c25d49c6c6fc3e7ebd3ba83b24d20356f2b73d
Rizky Hamdani <rizkyyhamdani21@gmail.com>
2025-02-10 14:43:41 +0800

    add a new rule for page expired

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+use Illuminate\Session\TokenMismatchException;
 
 class VerifyCsrfToken extends Middleware
 {
@@ -14,4 +16,30 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
     ];
+
+    public function handle($request, Closure $next)
+    {
+        try {
+            return parent::handle($request, $next);
+        } catch (TokenMismatchException $e) {
+            // If the request is AJAX
+            if ($request->ajax()) {
+                return response()->json([
+                    'message' => 'Session expired',
+                    'code' => 419
+                ], 419);
+            }
+            
+            // For regular requests, redirect to login page
+            if ($request->user()) {
+                auth()->logout();
+            }
+            
+            // You can also flash a message to the session
+            session()->flash('message', 'Your session has expired. Please log in again.');
+            
+            // Redirect to login page
+            return redirect()->route('login');
+        }
+    }
 }
