155aa3a62c4b85542996fafd1ac8e935146b2e93
Rizky Hamdani <rizkyyhamdani21@gmail.com>
2025-02-14 16:50:02 +0800

    format

--- resources/views/livewire/staff/leave.blade.php
+++ resources/views/livewire/staff/leave.blade.php
@@ -79,7 +79,7 @@
                     </div>
 
                     <div class="mt-auto">
-                        <a href="{{ asset('templates/Format izin Cuti.docx') }}" download
+                        <a href="{{ asset('templates/Format-izin-Cuti.docx') }}" download
                             class="flex items-center justify-center px-4 py-2 bg-blue-600 text-white text-sm font-medium rounded-lg hover:bg-blue-700 transition-colors">
                             <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
