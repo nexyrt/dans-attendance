f25c8825baea0110dd6bef771d264225aa411cc5
Rizky Hamdani <rizkyyhamdani21@gmail.com>
2025-04-08 13:03:43 +0800

    Add signature modal component with signature pad functionality

--- resources/views/livewire/manager/leave.blade.php
+++ resources/views/livewire/manager/leave.blade.php
@@ -1,20 +1,32 @@
-<!-- resources/views/livewire/manager/leave.blade.php -->
-<div class="max-w-6xl mx-auto py-6">
-    <!-- Put this where you want your "open modal" button -->
-    <button @click="$dispatch('open-modal', 'my-modal')">
-        Open Modal
-    </button>
-
-    <!-- Put this anywhere in your page -->
-    <x-modals.modal name="my-modal">
-        <div class="p-6">
-            <h2>This is my modal title</h2>
-            <p>This is the content of my modal</p>
-
-            <button @click="$dispatch('close-modal', 'my-modal')">
-                Close Modal
-            </button>
+<div>
+    <x-modals.modal name="signature-modal" maxWidth="md">
+        <div x-data="signaturePad()">
+            <h1 class="text-xl font-semibold text-gray-700 flex items-center justify-between">
+                <div>
+                    <canvas x-ref="signature_canvas" class="border rounded shadow">
+                    </canvas>
+                </div>
+            </h1>
         </div>
+        <button x-on:click="upload" class="text-white">
+            Submit
+        </button>
     </x-modals.modal>
 
+    <button @click="$dispatch('open-modal', 'signature-modal')" class="px-4 py-2 bg-blue-500 text-white rounded">
+        Sign Document
+    </button>
+
+    <script src="https://cdn.jsdelivr.net/npm/signature_pad@4.0.0/dist/signature_pad.umd.min.js"></script>
+
+    <script>
+        document.addEventListener('alpine:init', () => {
+            Alpine.data('signaturePad', () => ({
+                signaturePadInstance: null,
+                init() {
+                    this.signaturePadInstance = new SignaturePad(this.$refs.signature_canvas);
+                },
+            }))
+        })
+    </script>
 </div>
