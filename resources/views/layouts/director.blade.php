a34803c603ed609ab3a0c0c499d1e5a856c805d8
Rizky Hamdani <rizkyyhamdani21@gmail.com>
2025-06-19 17:00:37 +0800

    Add Livewire Flux package and integrate UI components in dashboard

--- resources/views/layouts/director.blade.php
+++ resources/views/layouts/director.blade.php
@@ -329,6 +329,7 @@
             </div>
         </div>
 
+        @fluxScripts
         @livewireScripts
         @livewireChartsScripts
         <script src="https://cdn.jsdelivr.net/npm/signature_pad@4.0.0/dist/signature_pad.umd.min.js"></script>
